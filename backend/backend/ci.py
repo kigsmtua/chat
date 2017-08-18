8d2a08b52a22a02c4f97f9dbed54e0f469b2bb35
John Kiragu <john.kiragu@jumo.world>
2017-08-18 11:32:40 +0300

    Separate settings in take to ensure travis uses the correct database

--- backend/backend/ci.py
+++ backend/backend/ci.py
@@ -1,1 +1,12 @@
-from .settings import * # noqa
+from .settings import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
