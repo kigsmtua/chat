29d6b8206e952f1dcf1dfa08ba32915642147189
John Kiragu <john.kiragu@jumo.world>
2017-08-28 17:46:16 +0300

    Build default components for application

---
+++ webfrontend/src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        AuthComponent,
+    ],
+    exports: [
+        AuthComponent,
+    ]
+})
+export class AuthModule {
+
+}
---
+++ webfrontend/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'home',
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.css']
+})
+export class HomeComponent {
+
+}
