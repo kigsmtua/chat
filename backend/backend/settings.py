b01139e39c2346cfdcb05ce2e81502a53bc62614
John Kiragu <mutuakiragu@gmail.com>
2017-09-18 20:19:07 +0300

    Build chat consumers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,4 +162,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
---
+++ backend/chat/consumers.py
@@ -0,0 +1,3 @@
+import json
+from channels import Channel
+from channels.auth import channel_session_user_from_http, channel_session_user
