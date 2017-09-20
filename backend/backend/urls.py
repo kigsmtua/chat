fffa46375e7a09a9c9341d0550ed8d19fc0b7822
John Kiragu <mutuakiragu@gmail.com>
2017-09-20 18:04:46 +0300

    Add urls and login view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.auth.views import login, logout
+from chat.views import index
 
 urlpatterns = [
+    url(r'^$', index),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout),
     url(r'^admin/', admin.site.urls),
     url(r'^profile/', include("profiles.urls", namespace='profiles')),
     url(r'^char/', include("chat.urls", namespace='chat')),
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,4 +1,7 @@
 """Chat application views."""
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
 from chat.models import Messages
 from chat.serializers import ThreadCreateSerializer
 from rest_framework.generics import CreateAPIView
@@ -11,3 +14,17 @@ class MessageCreateView(CreateAPIView):
     serializer_class = ThreadCreateSerializer
     queryset = Messages.objects.all()
     permission_classes = [AllowAny]
+
+
+@login_required
+def index(request):
+    """
+    Root page view.
+
+    This is essentially a single-page app, if you ignore the
+    login and admin parts.
+    """
+    rooms = {}
+    return render(request, "index.html", {
+        "rooms": rooms,
+    })
