d46d3993b3faf86475c39d58d2095df5f9e20ce8
John Kiragu <mutuakiragu@gmail.com>
2017-10-22 22:22:55 +0300

    Added send message method

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,4 +1,6 @@
 """Chat models."""
+import json
+
 from django.db import models
 
 from profiles.models import Profile
@@ -10,6 +12,21 @@ class Thread(models.Model):
     name = models.CharField(max_length=255, unique=True)
     createdAt = models.DateTimeField(auto_now=False, auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+    def send_message(self, message, user):
+        """Called to send a message to a user."""
+        msg = {
+            'thread_id': str(self.id),
+            'thread_name': str(self.id),
+            'message': message}
+
+        self.name.send(
+            # This just means we can
+            {"message": json.dumps(msg)}
+        )
 
 
 class Messages(models.Model):
