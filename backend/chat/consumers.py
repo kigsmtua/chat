af44ae8f9c82d616afa9b09fa7a44643596446fe
John Kiragu <mutuakiragu@gmail.com>
2017-12-07 13:55:26 +0300

    Add chat consumers for the basic chat system

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,3 +1,76 @@
+"""Message sending consumers for the chat system."""
 import json
+
 from channels import Channel
-from channels.auth import channel_session_user_from_http, channel_session_user
+from channels.auth import channel_session_user, channel_session_user_from_http
+
+
+@channel_session_user_from_http
+def ws_connect(message):
+    """Connect and Initialize User Session."""
+    message.reply_channel.send({'accept': True})
+    message.channel_session['messages'] = []
+
+
+def ws_receive(message):
+    payload = json.loads(message['text'])
+    payload['reply_channel'] = message.content['reply_channel']
+    Channel("chat.receive").send(payload)
+
+
+@channel_session_user
+def ws_disconnect(message):
+    for room_id in message.channel_session.get("rooms", set()):
+        try:
+            room = Room.objects.get(pk=room_id)
+            room.websocket_group.discard(message.reply_channel)
+        except Room.DoesNotExist:
+            pass
+
+
+@channel_session_user
+@catch_client_error
+def chat_join(message):
+
+    room = get_room_or_error(message["room"], message.user)
+
+    if NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS:
+        room.send_message(None, message.user, MSG_TYPE_ENTER)
+
+    room.websocket_group.add(message.reply_channel)
+    message.channel_session['rooms'] = list(set(message.channel_session['rooms']).union([room.id]))
+    # Send a message back that will prompt them to open the room
+    # Done server-side so that we could, for example, make people
+    # join rooms automatically.
+    message.reply_channel.send({
+        "text": json.dumps({
+            "join": str(room.id),
+            "title": room.title,
+        }),
+    })
+
+
+@channel_session_user
+@catch_client_error
+def chat_leave(message):
+    room = get_room_or_error(message["room"], message.user)
+
+    if NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS:
+        room.send_message(None, message.user, MSG_TYPE_LEAVE)
+
+    room.websocket_group.discard(message.reply_channel)
+    message.channel_session['rooms'] = list(set(message.channel_session['rooms']).difference([room.id]))
+    message.reply_channel.send({
+        "text": json.dumps({
+            "leave": str(room.id),
+        }),
+    })
+
+
+@channel_session_user
+@catch_client_error
+def chat_send(message):
+    if int(message['room']) not in message.channel_session['rooms']:
+        raise ClientError("ROOM_ACCESS_DENIED")
+    room = get_room_or_error(message["room"], message.user)
+    room.send_message(message["message"], message.user)
